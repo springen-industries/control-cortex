c0dac69f4082ac384da25130f013cbe925399012
Chris Spring <chris@nolaspring.net>
2017-10-16 14:45:36 -0400

    more oled

--- service.js
+++ service.js
@@ -5,7 +5,7 @@ var wire = new i2c(opts.address, {device: opts.device});
 
 var opts = {
   width: 128, // screen width
-  height; 32, // screen height
+  height: 32, // screen height
   address: 0x7B, // Pass I2C address of screen if it is not the default of 0x3C
   device: '/dev/i2c-1', // Pass your i2c device here if it is not /dev/i2c-1
   microview: true, // set to true if you have a microview display
@@ -13,7 +13,7 @@ var opts = {
 
 
 var oled = new oled(opts);
- 
+
 oled.turnOnDisplay();
 // draws 4 white pixels total
 // format: [x, y, color]
@@ -28,14 +28,4 @@ oled.drawPixel([
 wire.scan(function(err, data) {
   // result contains an array of addresses
   console.log(data);
-});
-
-
-var oled = new oled(opts);
-
-
-wire.read(length, function(err, res) {
-  // result contains a buffer of bytes
-  console.log(res.length);
-
 });
