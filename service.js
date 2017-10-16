e37418dd81cea6c001b17cd88d40d053330a975e
Chris Spring <chris@nolaspring.net>
2017-10-16 14:11:08 -0400

    just trying wire.read

--- service.js
+++ service.js
@@ -2,14 +2,8 @@ var i2c = require('i2c');
 var address = 0x0A;
 var wire = new i2c(address, {device: '/dev/i2c-1'}); // point to your i2c address, debug provides REPL interface
 
-wire.scan(function(err, data) {
-  // result contains an array of addresses
-  console.log(data);
-});
-
-
-
 wire.read(length, function(err, res) {
   // result contains a buffer of bytes
   console.log(res.length);
+
 });
