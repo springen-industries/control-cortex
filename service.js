ac5aca7d1efc85a0a68e0ce374e887c0760f1f87
Chris Spring <chris@nolaspring.net>
2017-10-16 14:35:11 -0400

    introduced oled library

--- service.js
+++ service.js
@@ -1,6 +1,26 @@
 var i2c = require('i2c');
-var address = 0x0A;
-var wire = new i2c(address, {device: '/dev/i2c-1'}); // point to your i2c address, debug provides REPL interface
+var oled = require('oled-ssd1306-i2c');
+
+
+var opts = {
+  width: 128, // screen width
+  height; 32, // screen height
+  address: 0x0A, // Pass I2C address of screen if it is not the default of 0x3C
+  device: '/dev/i2c-1', // Pass your i2c device here if it is not /dev/i2c-1
+  microview: true, // set to true if you have a microview display
+};
+
+
+var wire = new i2c(opts.address, {device: opts.device});
+
+wire.scan(function(err, data) {
+  // result contains an array of addresses
+  console.log(data);
+});
+
+
+var oled = new oled(opts);
+
 
 wire.read(length, function(err, res) {
   // result contains a buffer of bytes
