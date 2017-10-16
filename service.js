d045ae22e2378f476f60b922d7e1f5cec58c9072
Chris Spring <chris@nolaspring.net>
2017-10-16 17:00:10 -0400

    seg fault

--- service.js
+++ service.js
@@ -1,16 +1,41 @@
-var i2c = require('i2c');
-var oled = require('oled-ssd1306-i2c');
+'use strict';
 
+// When run, this program will output the same information as the
+// command 'i2cdetect -y -r 1'
+var fs = require('fs'),
+  i2c = require('../'),
+  i2c1 = i2c.openSync(1);
 
+var EBUSY = 16; /* Device or resource busy */
 
-var opts = {
-  width: 128, // screen width
-  height: 32, // screen heigh
-  address: 0x7B, // Pass I2C address of screen if it is not the default of 0x3C
-  device: '/dev/i2c-1', // Pass your i2c device here if it is not /dev/i2c-1
+function scan(first, last) {
+  var addr;
 
-};
-var wire = new i2c( {device: opts.device});
+  fs.writeSync(0, '     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f');
 
-console.log(wire.scanSync());
-  // result contains an array of addresses
+  for (addr = 0; addr <= 127; addr += 1) {
+    if (addr % 16 === 0) {
+      fs.writeSync(0, '\n' + (addr === 0 ? '0' : ''));
+      fs.writeSync(0, addr.toString(16) + ':');
+    }
+
+    if (addr < first || addr > last) {
+      fs.writeSync(0, '   ');
+    } else {
+      try {
+        i2c1.receiveByteSync(addr);
+        fs.writeSync(0, ' ' + addr.toString(16)); // device found, print addr
+      } catch (e) {
+        if (e.errno === EBUSY) {
+          fs.writeSync(0, ' UU');
+        } else {
+          fs.writeSync(0, ' --');
+        }
+      }
+    }
+  }
+
+  fs.writeSync(0, '\n');
+}
+
+scan(0x3, 0x7F);
