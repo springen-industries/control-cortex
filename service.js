9548d75257a0057c8f46778bac6c9f0126b37259
Chris Spring <chris@nolaspring.net>
2017-10-16 13:43:34 -0400

    changed address and i2c device id

--- service.js
+++ service.js
@@ -1,6 +1,6 @@
 var i2c = require('i2c');
-var address = 0x7B;
-var wire = new i2c(address, {device: '/dev/i2c-1'}); // point to your i2c address, debug provides REPL interface
+var address = 0x0A;
+var wire = new i2c(address, {device: '/dev/i2c-0'}); // point to your i2c address, debug provides REPL interface
 
 wire.scan(function(err, data) {
   // result contains an array of addresses
