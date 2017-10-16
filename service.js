2aceeaac9b0f9e8b5ee63785be5a252079e8f905
Chris Spring <chris@nolaspring.net>
2017-10-16 11:23:14 -0400

    syntax

--- service.js
+++ service.js
@@ -1,10 +1,9 @@
 var i2c = require('i2c');
-var address = 0x78;
+var address = 0x7B;
 var wire = new i2c(address, {device: '/dev/i2c-1'}); // point to your i2c address, debug provides REPL interface
 
 wire.scan(function(err, data) {
   // result contains an array of addresses
-
 });
 
 wire.writeByte(byte, function(err) {});
