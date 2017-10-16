eb6fec6ecf241bca3a70d90eeb2c8fc33c07ae0a
Chris Spring <chris@nolaspring.net>
2017-10-16 16:35:16 -0400

    null object issue

--- service.js
+++ service.js
@@ -1,16 +1,16 @@
 var i2c = require('i2c');
 var oled = require('oled-ssd1306-i2c');
-var wire = new i2c(opts.address, {device: opts.device});
+
 
 
 var opts = {
   width: 128, // screen width
-  height: 32, // screen height
+  height: 32, // screen heigh
   address: 0x7B, // Pass I2C address of screen if it is not the default of 0x3C
   device: '/dev/i2c-1', // Pass your i2c device here if it is not /dev/i2c-1
 
 };
-
+var wire = new i2c(opts.address, {device: opts.device});s
 
 var oled = new oled(opts);
 
