c07bb8d404aa679969465cbda5e62c4e0b5cf079
Chris Spring <chris@nolaspring.net>
2017-10-16 14:08:53 -0400

    added read statment to check if atmega sees the request

--- service.js
+++ service.js
@@ -1,12 +1,15 @@
 var i2c = require('i2c');
 var address = 0x0A;
-<<<<<<< HEAD
 var wire = new i2c(address, {device: '/dev/i2c-1'}); // point to your i2c address, debug provides REPL interface
-=======
-var wire = new i2c(address, {device: '/dev/i2c-0'}); // point to your i2c address, debug provides REPL interface
->>>>>>> 849936322467ad9f37403edfc6b6594448ed38a7
 
 wire.scan(function(err, data) {
   // result contains an array of addresses
   console.log(data);
+});
+
+
+
+wire.read(length, function(err, res) {
+  // result contains a buffer of bytes
+  console.log(res.length);
 });
