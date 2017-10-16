5f4ea031434391331d40e1ae15a902e0bcafac76
Chris Spring <chris@nolaspring.net>
2017-10-16 11:20:44 -0400

    i2c address of oled

--- service.js
+++ service.js
@@ -1,10 +1,10 @@
 var i2c = require('i2c');
-var address = 0x18;
+var address = 0x78;
 var wire = new i2c(address, {device: '/dev/i2c-1'}); // point to your i2c address, debug provides REPL interface
 
 wire.scan(function(err, data) {
   // result contains an array of addresses
-  console.write(data);
+
 });
 
 wire.writeByte(byte, function(err) {});
