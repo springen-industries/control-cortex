01b640ee715a3919e98f1d19cc3e2b3f89edd3f0
Chris Spring <chris@nolaspring.net>
2017-10-17 13:52:57 -0400

    first attempt to read over i2c from pi

--- service.js
+++ service.js
@@ -6,12 +6,14 @@ var i2c = require("i2c-bus");
 var i2cUtils = require('./lib/i2c-utils');
 new i2cUtils().scan(0x0, 0x7F);
 
+function onResult(results){
+  console.log(result);
+}
 
 var i2c1 = i2c.open();
-var result = i2c1.readByte(0x8f, 6, function (err, config) {
-          if (err) return cb(err);
-          if (config & 0x10) return read();
-          cb(null);
-        });
 
-console.log(result);
+var result = i2c1.readByte(0x8f, 6, function (err, config) {
+          if (err) return onResult(err);
+          if (config & 0x10) return read();
+          onResult(null);
+        });
