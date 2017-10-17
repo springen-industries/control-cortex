e32e296c88475ad5d334b5ecd9ebf7dc593d5b87
Chris Spring <chris@nolaspring.net>
2017-10-17 13:39:21 -0400

    first attempt to read over i2c from pi

--- service.js
+++ service.js
@@ -1,5 +1,11 @@
 'use strict';
 
-var i2cUtils = require('./lib/i2c-utils');
+var i2c = require("i2c-bus");
 
+
+var i2cUtils = require('./lib/i2c-utils');
 new i2cUtils().scan(0x0, 0x7F);
+
+
+var i2c1 = i2c.openSync(1);
+i2c1.readByteSync(0x7F,6);
