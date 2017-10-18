859a587a0218c05ae4720bb3a750e0dd59a58758
Chris Spring <chris@nolaspring.net>
2017-10-18 14:38:28 -0400

    added read function

--- service.js
+++ service.js
@@ -1,8 +1,7 @@
 'use strict';
-var i2c = require("i2c-bus");
 var i2cUtils = require('./lib/i2c-utils');
+var i2cFace = new i2cUtils();
 
-i2cFace = new i2cUtils();
 i2cFace.scan(0x0, 0x7F);
 //read six bytes of data from 0x9A
 var output = i2cFace.readMessage(0x9A,6);
