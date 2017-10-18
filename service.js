d548a387776feff1fd69f545e605121576b806d2
Chris Spring <chris@nolaspring.net>
2017-10-17 22:49:15 -0400

    tweaking i2c

--- service.js
+++ service.js
@@ -6,6 +6,6 @@ new i2cUtils().scan(0x0, 0x7F);
 
 var GIMBAL_1_ADDRESS = 0x8F;
 
-var i2c1 = i2c.openSync(1);
+var i2c1 = i2c.openSync(15);
 while ((i2c1.readByteSync(GIMBAL_1_ADDRESS, 6) & 0x8F) === 0) {
 }
