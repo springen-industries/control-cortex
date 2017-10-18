0d7ae5785092420fda4f54f3d11dfbc0bb81d3c9
Chris Spring <chris@nolaspring.net>
2017-10-17 22:34:56 -0400

    removed the raspi-i2c library

--- service.js
+++ service.js
@@ -3,3 +3,8 @@ var i2c = require("i2c-bus");
 var i2cUtils = require('./lib/i2c-utils');
 
 new i2cUtils().scan(0x0, 0x7F);
+
+var GIMBAL_1_ADDRESS = 0x8F;
+
+var i2c1 = i2c.open(1);
+i2c1.i2cRead(GIMBAL_1_ADDRESS,6);
