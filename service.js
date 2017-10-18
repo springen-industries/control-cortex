24b0577b82a2f643d77e2bf3c5659c0d0fb5caef
Chris Spring <chris@nolaspring.net>
2017-10-17 22:39:31 -0400

    tweaking i2c

--- service.js
+++ service.js
@@ -4,7 +4,7 @@ var i2cUtils = require('./lib/i2c-utils');
 
 new i2cUtils().scan(0x0, 0x7F);
 
-var GIMBAL_1_ADDRESS = F;
+var GIMBAL_1_ADDRESS = 0x8F;
 
 var i2c1 = i2c.open(1);
 i2c1.i2cRead(GIMBAL_1_ADDRESS,6);
