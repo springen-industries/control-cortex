da0f90d4d61b40b1d3b01531f0577b73973fa0b2
Chris Spring <chris@nolaspring.net>
2017-10-18 10:14:37 -0400

    stuff with I2C

--- service.js
+++ service.js
@@ -6,6 +6,6 @@ new i2cUtils().scan(0x0, 0x7F);
 
 var GIMBAL_1_ADDRESS = 0x8F;
 
-var i2c1 = i2c.openSync(15);
-var output = i2c1.i2cReadSync(8,6, function() {} );
+var i2c1 = i2c.openSync(0x8F);
+var output = i2c1.i2cReadSync(0x8f,6, function() {} );
 console.log(output);
