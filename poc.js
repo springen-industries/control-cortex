4e699663f8d75a0589de878783b33042570cdeca
Chris Spring <chris@nolaspring.net>
2017-10-20 14:10:55 -0400

    buffer stuff

--- poc.js
+++ poc.js
@@ -3,12 +3,12 @@ var i2cUtils = require('./lib/i2c-utils');
 var sleep = require("sleep");
 var i2cFace = new i2cUtils();
 
-i2cFace.scan(0x0, 0x7F);
-
-i2cFace.listAddresses();
+// i2cFace.scan(0x0, 0x7F);
+//
+//i2cFace.listAddresses();
 
 while(true) {
-  var output = i2cFace.readModuleState(26,5);
+  var output = i2cFace.readModuleState(26,4);
   console.log(output);
   sleep.msleep(40);
 }
