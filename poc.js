faca1c0753f8b02a8890c866744ff9209b37a11d
Chris Spring <chris@nolaspring.net>
2017-11-17 16:31:00 -0500

    added normalization function for i2c-gimbal output

--- poc.js
+++ poc.js
@@ -3,18 +3,35 @@ var i2cUtils = require('./lib/i2c-utils');
 var sleep = require("sleep");
 var i2cFace = new i2cUtils();
 
-// i2cFace.scan(0x0, 0x7F);
-//
-//i2cFace.listAddresses();
+
+var deadband_minimum = 250;
+var deadband_maximum = 10;
+
+var axisMinimum = [200,200,200,200];
+var axisMaximum = [100,100,100,100];
+
 
 while(true) {
   var output =  i2cFace.readModuleState(26,4);
-  i2cFace.writeToRadio(8,byteToPPM((output);
+  i2cFace.writeToRadio(8,normalizeBytes(output));
 
 }
 
 
 // Takes readings from i2c-gimbal and shifts them to 0-255 for easier conversion to PPM
-function normalizeBytes(byte) {
-  return byte;
+function normalizeBytes(bytes) {
+  for(var i=0;i<bytes.length;i++){
+    if (bytes[i] > axisMinimum[i] && bytes[i] < deadband_minimum  ){
+      bytes[i] = map_range(bytes[i],axisMinimum[i],255,0,128);
+    } else if(bytes[i] > deadband_maximum) {
+      bytes[i] = map_range(bytes[i],axisMinimum[i],255,128,256);
+    } else {
+      bytes[i] = 128;
+    }
+  }
+  return bytes;
+}
+
+function map_range(value, low1, high1, low2, high2) {
+    return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
 }
