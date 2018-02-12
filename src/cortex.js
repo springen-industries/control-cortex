12aa7fe0d8a1786ed988ff04e62e10e57b9f0f13
Christopher Spring <chris@nolaspring.net>
2018-02-11 21:46:01 -0600

    dumped all code from cortex

--- src/cortex.js
+++ src/cortex.js
@@ -1,10 +1,1 @@
 'use strict';
-var i2cUtils = require('./lib/i2c-utils');
-var sleep = require("sleep");
-var i2cFace = new i2cUtils();
-
-
-while(true) {
-  var output =  igit 2cFace.readModuleState(26,4);
-  i2cFace.writeToRadio(8,output);
-}
