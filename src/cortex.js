4bcc261114eb428cc17bfa25076838730f2d49c7
Christopher Spring <chris@nolaspring.net>
2018-02-13 12:51:01 -0600

    Promise pattern for moduleFactory implemented

--- src/cortex.js
+++ src/cortex.js
@@ -5,6 +5,11 @@ var config = require('../configs/controller-config.json');
 console.log("Read configuration: ~/configs/controller-config.json and found " + config.modules.length + " modules")
 //console.log(beautify(config));
 
-config.modules.forEach( mod => {
-  moduleFactory.loadModule(mod, (res,err) => {console.log("done")});
-});
+function begin(){
+  console.log("Starting main loop");
+}
+
+// this will launch an instance and callback for each module found.
+// this should be wrapped in a wait() that blocks until each module reports sucess
+// module factory should return a promise here and get passed all modules as a set
+moduleFactory.loadModules(config.modules,begin);
--- src/module-factory.js
+++ src/module-factory.js
@@ -1,6 +1,26 @@
 
 moduleFactory = {};
-moduleFactory.loadModule = function(mod, done){
-  console.log("Loading module:" + mod.name);
+
+// do this with promises
+moduleFactory.loadModules = function(moduleConfigs, done){
+  promises = [];
+  moduleConfigs.forEach( moduleConfig => {
+    promises.push(moduleFactory.loadModule(moduleConfig));
+  });
+
+  Promise.all(promises).then(done).catch(ex => {console.log(ex)});
 }
+////////////
+moduleFactory.loadModule = (moduleConfig) => {
+  return new Promise(function(resolve,reject) {
+
+        console.log("Loading Module:" + moduleConfig.name);
+        if (true){
+          return resolve(moduleConfig);
+        }
+        reject("Blah");
+
+  });
+}
+//////////////
 module.exports = moduleFactory;
