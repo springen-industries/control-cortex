a4a5dc7d802f2a8860603d6209a34959aed6f6af
Christopher Spring <chris@nolaspring.net>
2018-02-13 10:13:07 -0600

    Parsing list into descrete modules

--- src/cortex.js
+++ src/cortex.js
@@ -1,7 +1,12 @@
 'use strict';
 require('./shared-services.js');
 
-var modules = require('../configs/controller-config.json');
+var config = require('../configs/controller-config.json');
 
 console.log("Read configuration: ~/configs/controller-config.json")
-console.log(beautify(modules));
+console.log(beautify(config));
+
+config.modules.forEach( module => {
+  console.log("Module:" + module.name);
+  console.log(beautify(module));
+});
