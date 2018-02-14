0b42a6516a20414ddb276d8ae4d521e98fcb758b
Christopher Spring <chris@nolaspring.net>
2018-02-13 18:07:30 -0600

    Progress around device protoyping

--- src/lib/i2c-device.js
+++ src/lib/i2c-device.js
@@ -1,31 +1,39 @@
 // When run, this program will output the same information as the
 // command 'i2cdetect -y -r 1'
-
- {
-
 var fs = require('fs'),
-  i2c = require('i2c-bus'),
-  i2c1 = i2c.openSync(1);
-  var EBUSY = 16; /* Device or resource busy */
+i2c = require('i2c-bus'),
+
+
+const var EBUSY = 16; /* Device or resource busy */
 
 
 
+/*
+  address - address the device is listening on
+  pull - indicates that device needs to be activly probe to deliver data
+  push - indicates that the i2c device will raise an event on the master when it wants to send stuff
 
-this.readModuleState = (moduleAddress,length) => {
+*/
+var i2cDevice = {};
+i2cDevice.i2cAddress = "";
+i2cDevice.sampleType = "push";
+
+
+i2cDevice.readMessage = (buffer,length) => {
   var buf = new Buffer(length);
   i2c1.i2cReadSync(moduleAddress,length,buf);
   return buf;
 }
 
-this.writeToRadio = (radioAddress,value) => {
+i2cDevice.writeMessage = (value,complete) => {
     try {
-      i2c1.i2cWriteSync(radioAddress,value.length,value);
+      i2c1.i2cWrite(this.i2cAddress,value.length,value,complete);
     }  catch(exception){
       console.log(exception);
     }
 }
 
 
-}
 
-module.exports = i2cUtils;
+
+module.exports = i2cDevice;
